1dffb9683f96c179e4b92d3756b5728d5cd84e2d
SedgwickRichardson <michiel@sedgwick-richardson.com>
2013-12-06 16:25:38 +0800

    snowfall + music

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
 
-/* 	$("#splash").delay(3000).fadeOut(); */
-/* 	$("#city-select").delay(2500).fadeIn() */
 
+	// initiate snow fall
+	$.fn.snow({ minSize: 10, maxSize: 30, newOn: 500, flakeColor: '#fff' });
+	
+	// initiate music
+	soundManager.setup({
+	  url: 'assets/swf/',
+	  onready: function() {
+	    var mySound = soundManager.createSound({
+	      id: 'aSound',
+	      url: 'assets/mp3/opt2.mp3'
+	    });
+	    mySound.play();
+	  },
+	  ontimeout: function() {
+	    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+	  }
+	});
 	
 });
