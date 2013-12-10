03b511307ab0ac585c89f241411217ecd42ebf2f
SedgwickRichardson <michiel@sedgwick-richardson.com>
2013-12-10 10:25:42 +0800

    added IE6 check

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,31 +2,43 @@ $(document).ready(function() {
 
 
 	// initiate snow fall
-	$.fn.snow({ minSize: 20, maxSize: 45, newOn: 800, flakeColor: '#fff' });
-	
-	// initiate music
-	soundManager.setup({
-	  url: 'assets/swf/',
-	  onready: function() {
-	    var mySound = soundManager.createSound({
-	      id: 'aSound',
-	      url: 'assets/mp3/opt1.mp3'
-	    });
-	    mySound.play();
-	  },
-	  ontimeout: function() {
-	    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
-	  }
+/*
+	$(document).snowfall({
+		image :"assets/img/snowflake.png", 
+		minSize: 10, 
+		maxSize:32,
+		flakeCount: 20,
+		maxSpeed: 2
 	});
+*/
+	if ($("html").hasClass("lt-ie8")) {
 	
-	//colorbox
-	$(".clicktags a").colorbox({
-			inline:true, 
-			//width:"740px", 
-			//height:"500px",
-			opacity: 0,
-			transition: "none"
+	} else {
+		$.fn.snow({ minSize: 20, maxSize: 45, newOn: 300, flakeColor: '#fff' });
 		
-	});
-	
+		// initiate music
+		soundManager.setup({
+		  url: 'assets/swf/',
+		  onready: function() {
+		    var mySound = soundManager.createSound({
+		      id: 'aSound',
+		      url: 'assets/mp3/opt1.mp3'
+		    });
+		    mySound.play();
+		  },
+		  ontimeout: function() {
+		    // Hrmm, SM2 could not start. Missing SWF? Flash blocked? Show an error, etc.?
+		  }
+		});
+		
+		//colorbox
+		$(".clicktags a").colorbox({
+				inline:true, 
+				//width:"740px", 
+				//height:"500px",
+				opacity: 0,
+				transition: "none"
+			
+		});
+	}
 });
